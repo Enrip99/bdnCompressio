a9dec713e512d6f36f2ffd6a138c4deee40126a6
enrip99 <enric.1999@hotmail.com>
2024-10-02 18:25:16 +0200

    Escriptura migrada a classe

--- Compressor.cpp
+++ Compressor.cpp
@@ -1,31 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string.h>
-
-std::ofstream out("sortida.bdn");
-__uint8_t byteActual = 0;
-const int numBits = sizeof(byteActual) * __CHAR_BIT__;
-unsigned char comptadorBit = 0;
-
-void escriuBit(bool bit){
-    byteActual <<= 1;
-    if (bit) ++byteActual;
-    ++comptadorBit;
-    if (!(comptadorBit % numBits)){
-        out << byteActual;
-    }
-}
-
-void acabaEscriure(){
-    if (comptadorBit % numBits){
-        while (comptadorBit % numBits){
-            byteActual <<= 1;
-            ++comptadorBit;
-        }
-        out << byteActual;
-    }
-    out.close();
-}
+#include "EscriptorBits.h"
 
 int main(int argc, char *argv[]){
     
@@ -35,14 +11,15 @@ int main(int argc, char *argv[]){
         return 1;
     }
     */
+   EscriptorBits escriptor("sortida.bdn");
 
     std::string input;
     while (std::cin >> input){
         for (int i = 0; i < input.length(); ++i){
-            escriuBit(input[i] != '0');
+            escriptor.escriuBit(input[i] != '0');
         }
     }
-    acabaEscriure();
+    escriptor.acabaEscriure();
     
     return 0;
 }
---
+++ EscriptorBits.cpp
@@ -0,0 +1,29 @@
+#include "EscriptorBits.h"
+
+EscriptorBits::EscriptorBits(std::string fitxer){
+    byteActual = 0;
+    comptadorBit = 0;
+    sortida.open( fitxer , std::ios::out | std::ios::trunc | std::ios::binary);
+};
+
+void EscriptorBits::escriuBit(bool bit){
+    byteActual <<= 1;
+    if (bit) ++byteActual;
+    ++comptadorBit;
+    if (!(comptadorBit % numBits)){
+        sortida << byteActual;
+    }
+    
+};
+
+void EscriptorBits::acabaEscriure(){
+    if (comptadorBit % numBits){
+        while (comptadorBit % numBits){
+            byteActual <<= 1;
+            ++comptadorBit;
+        }
+        sortida << byteActual;
+    }
+    sortida.close();
+
+};
