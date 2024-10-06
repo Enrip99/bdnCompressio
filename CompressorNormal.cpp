a53fdfd9f9d33773dbdc8d1b2d6c6e30ce31143a
enrip99 <enric.1999@hotmail.com>
2024-10-06 20:27:25 +0200

    Escriptura de l'arbre a disc

--- CompressorNormal.cpp
+++ CompressorNormal.cpp
@@ -8,6 +8,26 @@
 
 #define midaBuffer 4096
 
+void escriuArbre(const BinTree<__uint8_t> & arbre, EscriptorBits & sortida)
+{
+    if (arbre.left().empty()){
+        sortida.escriuBit(1);
+        sortida.escriuBit(arbre.value() & 0b10000000);
+        sortida.escriuBit(arbre.value() & 0b01000000);
+        sortida.escriuBit(arbre.value() & 0b00100000);
+        sortida.escriuBit(arbre.value() & 0b00010000);
+        sortida.escriuBit(arbre.value() & 0b00001000);
+        sortida.escriuBit(arbre.value() & 0b00000100);
+        sortida.escriuBit(arbre.value() & 0b00000010);
+        sortida.escriuBit(arbre.value() & 0b00000001);
+    }
+    else{
+        sortida.escriuBit(0);
+        escriuArbre(arbre.left(),  sortida);
+        escriuArbre(arbre.right(), sortida);
+    }
+}
+
 int main(int argc, char *argv[]){
     /*
     if (argc != 3){
@@ -17,18 +37,32 @@ int main(int argc, char *argv[]){
     */
 
     unsigned long long int llistatOcurrencies[bytesDiferents] = {0};
+    //Per a cada byte 0x00 - 0xFF, comptador de quants cops surt.
 
     FILE * entrada = fopen(argv[1], "r");
     __uint8_t bufferLectura[midaBuffer];
     int bytesLlegits;
+    //Obrim fitxer a llegir, primer argument de la linia de comanda.
+    //Variables per assistir a la lectura a baix nivell.
 
     std::multimap<unsigned long long int, BinTree<__uint8_t> > insercions;
     std::multimap<unsigned long long int, BinTree<__uint8_t> >::iterator iterador;
     unsigned long long int tempOcurrencia;
     BinTree<__uint8_t> tempArbre;
+    //Mapa auto-ordenat amb les fulles o sub-arbres que formaran el huffman.
 
-    //EscriptorBits escriptor(argv[2]);
-    //EscriptorBits escriptor("sortida2.bdn");
+    BinTree<__uint8_t> ArbreHuffman;
+    //Arbre final.
+
+    EscriptorBits escriptor(argv[2]);
+    //Objecte per escriure l'arbre i fixter comprimit a disc
+
+
+
+    //Llistat amb les freqüències de cada byte.
+    //
+    //Recorrem el fitxer original sencer.
+    //Per cada ocurrència d'un byte, s'incrementa en 1 el seu comptador a l'array.
 
     while ((!std::feof(entrada)) && (bytesLlegits = fread(bufferLectura, sizeof(__uint8_t), midaBuffer, entrada))){
         for (int i = 0; i < bytesLlegits; ++i){
@@ -36,7 +70,12 @@ int main(int argc, char *argv[]){
         }
     }
 
-    //Construir arbre
+
+    //Construir arbre.
+    //
+    //Per cada element no zero al llistat de freqüències es crea un element d'un multimap.
+    //Cada element s'identifica per la freqüència, i compté un arbre (només fulla) amb el byte
+    //al que correspon.
 
     for (unsigned int i = 0; i < bytesDiferents; ++i){
         if (llistatOcurrencies[i]){
@@ -47,6 +86,11 @@ int main(int argc, char *argv[]){
         }
     }
 
+    //Reiteradament, agafem els dos primers elements del mapa i fusionem els seus dos arbres.
+    //Eliminem aquests dos elements i fiquem el nou arbre, amb la freqüència essent la
+    //suma dels dos sub-arbres originals. Repetim fins que només en quedi un.
+    //Desem l'arbre final a ArbreHuffman.
+
     iterador = insercions.begin();
     while (insercions.size() > 1){
         tempOcurrencia = iterador -> first;
@@ -54,27 +98,36 @@ int main(int argc, char *argv[]){
         iterador = insercions.erase(iterador);
         insercions.insert({
             tempOcurrencia + iterador -> first,
-            BinTree<__uint8_t>(0, tempArbre, iterador -> second)
+            BinTree<__uint8_t>(0xFF, tempArbre, iterador -> second)
             });
         iterador = insercions.erase(iterador);
     }
-
-    
-
-    /*
-    for (const auto& e : insercions){
-        std::cout << e.first << " " << e.second.value() << std::endl;
-    }
-    */
+    ArbreHuffman = iterador -> second;
 
 
-    //Escriure Arbre
-        // recorda que no cal escriure res de les freqüències
+    //Escriure arbre a disc
+    //
+    //Escrivim l'arbre en inordre. Un 0 significa baixar per la branca esquerra.
+    //Un 1 significa la presència d'una fulla. Després de cada 1 venen 8 bits representant
+    //el byte que codifiquen. Després de cada fulla, es retrocedeix al node pare i es
+    //continua per la branca dreta, sense cap bit que ho indiqui (recodem que en un arbre de
+    //Huffman, tots el nodes tenen 0 o 2 branques, mai 1).
+    //
+    //       arrel
+    //       /  \
+    //      -    -
+    //     / \  / \
+    //     A B  C -
+    //           / \
+    //           D E
+    //
+    //Es representaria com 0 0 1(A) 1(B) 0 1(C) 0 1(D) 1(E)
 
+    escriuArbre(ArbreHuffman, escriptor);
 
 
     //Llegir un altre cop el fitxer i comprimir-ho
-    /*
+    
     std::fseek(entrada, 0, SEEK_SET);
     std::clearerr(entrada);
     while ((!std::feof(entrada)) && (bytesLlegits = fread(bufferLectura, sizeof(__uint8_t), midaBuffer, entrada))){
@@ -84,7 +137,7 @@ int main(int argc, char *argv[]){
     }
     escriptor.acabaEscriure();
     std::fclose(entrada);
-    */
+    
     
     return 0;
 }
